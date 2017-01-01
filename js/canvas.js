c16360308b415ad8053f4ee11a3ac689f1a96e1f
Esteban Padilla <ep@estebanpadilla.com>
2017-01-01 01:10:51 -0600

    Save

---
+++ js/canvas.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', init, false);
+function init() {
+
+    var canvas = undefined;
+    var context = undefined;
+    canvas = document.createElement('canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.style.position = 'absolute';
+    canvas.style.left = '0px';
+    canvas.style.top = '0px';
+    canvas.style.background = '#ee3344';
+
+    document.body.appendChild(canvas);
+
+    context = canvas.getContext('2d');
+
+    context.fillStyle = 'yellow';
+    context.fillRect(100, 100, 100, 100);
+}
