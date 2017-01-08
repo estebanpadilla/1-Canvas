ff4e5a7a8fc4791fa45ed351273ee7a7e8ae1575
Esteban Padilla <ep@estebanpadilla.com>
2017-01-08 11:11:02 -0600

    Save changes.

--- js/canvas.js
+++ js/canvas.js
@@ -5,21 +5,23 @@ How to set canvas properties and style.
 What is the 2d context.
 */
 
+//Use the load event handler to start the canvas, its is better this way to allow the browser to load any files needed, like sound, images.
 window.addEventListener('load', init, false);
+
 function init() {
 
-    var canvas = undefined;
-    var context = undefined;
+    var canvas = null;
+    var context = null;
+
+    //Create the canvas object amd add it to the document body.
     canvas = document.createElement('canvas');
+    document.body.appendChild(canvas);
+    canvas.style.position = 'absolute';
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    canvas.style.position = 'absolute';
-    canvas.style.left = '0px';
-    canvas.style.top = '0px';
     canvas.style.background = '#ee3344';
 
-    document.body.appendChild(canvas);
-
+    //Gett the 2d context from the canvas
     context = canvas.getContext('2d');
 
     context.fillStyle = 'yellow';
